c13d8ca0cf9995fbc8891e6fe825a02081abe663
recafox <recafox@gmail.com>
2021-06-13 23:05:00 +0800

    fix key error

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 // figure out what set of credentials to return
 if (process.env.NODE_ENV === "production") {
-  module.exports = require("./dev.key");
+  module.exports = require("./prod.key");
 } else {
   module.exports = require("./dev.key");
 }
