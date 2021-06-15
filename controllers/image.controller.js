8aa49ca2838aad70051e46bcc107d1cd65928807
recafox <recafox@gmail.com>
2021-06-15 22:50:10 +0800

    resize image before upload

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -1,9 +1,16 @@
 const ImageModel = require("../models/image.model");
+const sharp = require("sharp");
 
 exports.uploadImage = async (req, res, next) => {
   try {
+    // resize image to 150x150
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 150, height: 150 })
+      .png()
+      .toBuffer();
+
     const createdModel = await ImageModel.create({
-      file: req.file.buffer,
+      file: buffer,
     });
     res.status(201).json(createdModel);
   } catch (error) {
